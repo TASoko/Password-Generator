8e37ad9852d8f3e6a170a686c02e50865a543e78
Thokozile Soko <thokozile.soko@yahoo.com>
2020-11-20 22:17:56 -0500

    This is the final.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Password-Generator
+
+Project:An employee with access to sensitive data wants to randomly generate a password that meets certain criteria so that they can create a strong password that provides greater security
+
+This respoitory consists of 3 files; html, javascript and css. For this week's homework we were given the starting code. The html and css files were not altered from the original in anyway. There were a few bits of code in the javascript file to create a button however everything else was coded in. The project successfully allows a user to generate a random password that meets their criteria. It offers 8 - 128 characters of length, upper and lower case letters, numbers and special characters. Once the specifications are chosen the password will display in the text box.
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
+//Click generate password button 
+var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
@@ -9,56 +10,62 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  function generatePassword() {
-
-    var length = prompt(
-      "How many characters do you wish to have?"
-    );
-    if (length < 8 || length > 128) {
-      alert("You have chosen " + length, "many words");
-      var passwordLow = confirm("Do you want to include lower case letters?");
-      var passwordUpp = confirm("Do you want to include upper case letters?");
-      var passwordNum = confirm("Do you want to include numbers?");
-      var passwordSpe = confirm("Do you want to include special characters?");
-    } else { 
-      (length > 7 || length < 129) 
-      alert("Not a valid number");
-    }
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#$%&'()*+-./:;<=>?@[\]^_`{|}~",
-      retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
-  }
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//Click generate password button 
-
-
+function generatePassword() {
 //Call function to generate passwords
   //Presented with a series of prompts for password criteria
-
-
-//Cycle through the criteria
-
+  var length = prompt(
+    "How many characters do you wish to have?"
+  );
+  //Cycle through the criteria
   //Choose a length of at least 8 characters and no more than 128 characters
-
-
+  if (length >= 8 && length <= 128) {
+    alert(" You have chosen " + length + " characters ");
+    let passwordLow = confirm("Do you want to include lower case letters?");
+    let passwordUpp = confirm("Do you want to include upper case letters?");
+    let passwordNum = confirm("Do you want to include numbers?");
+    let passwordSpe = confirm("Do you want to include special characters?");
   //Choose lowercase, uppercase, numeric, and/or special characters
+      charset = "";
+      password = "";
 
-
-
-//Return password from the generate password button
+    if (passwordLow) {
+      charset += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (passwordUpp) {
+      charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (passwordNum) {
+      charset += "0123456789";
+    }
+    if (passwordSpe) {
+      charset += "#$%&'()*+-./:;<=>?@[]\^_`{|}~";
+    }
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    //Return password from the generate password button
   //Input should be validated and at least one character type should be selected
   //Password is generated that matches the selected criteria
-
-
-
+    return password;
+  }
+  else {
+    alert("You password must be between 8 and 128 characters.");
+  }
+}
+// Add event listener to generate button
 //Add the password to the screen
   //Password is either displayed in an alert or written to the page
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
